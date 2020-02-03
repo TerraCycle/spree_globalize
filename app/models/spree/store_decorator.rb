79525d787dfd7e445b566b09f9ca38f17b646712
Adam Foldvari <>
2020-02-03 14:45:32 +0100

    Add versioning

--- app/models/spree/store_decorator.rb
+++ app/models/spree/store_decorator.rb
@@ -1,6 +1,6 @@
 module Spree::StoreDecorator
   def self.prepended(base)
-    base.translates :name, :meta_description, :meta_keywords, :seo_title, fallbacks_for_empty_translations: true
+    base.translates :name, :meta_description, :meta_keywords, :seo_title, fallbacks_for_empty_translations: true, versioning: :paper_trail
   end
 
   Spree::Store.include SpreeGlobalize::Translatable
