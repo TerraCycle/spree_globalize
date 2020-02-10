459aed382def37d5ec1b1d5c7e4d89c5d178121f
Janos Seller <seller.janos@gmail.com>
2020-02-10 16:54:49 +0100

    Use spree default current_store instead of ::Spree::Store.current

--- app/overrides/spree/admin/shared/_translations/translation.rb
+++
@@ -1,8 +0,0 @@
-Deface::Override.new(
-  virtual_path:  'spree/admin/shared/_translations',
-  name:          'translations_translation',
-  insert_bottom: 'script',
-  text:           <<-HTML
-                    $.extend(Spree.translations, { translations: "<%= strip_tags(Spree.t(:'i18n.translations')) %>" })
-                  HTML
-)
